c429299a526b6014e2bae129a4476ac365e973b3
KumaXOXO <lucas.schneider2@gmx.de>
2026-05-19 23:24:43 +0200

    chore: scaffold project structure (shared/server/client)

---
+++ client/vite.config.ts
@@ -0,0 +1,13 @@
+﻿import { defineConfig } from "vite"
+import { resolve } from "path"
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@twinky/shared": resolve(__dirname, "../shared"),
+    },
+  },
+  server: {
+    port: 5173,
+  },
+})
